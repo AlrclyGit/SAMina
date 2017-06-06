2a322c6b2da5bddcd64709eacd3d5dea591565dd
萧俊介 <Alrcly@foxmail.com>
2017-06-06 15:40:51 +0800

    循环绑定
    利用循环进行绑定操作

--- Dome1/pages/posts/post.js
+++ Dome1/pages/posts/post.js
@@ -12,19 +12,34 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var post_content1={
-      data:"Sep 18 2016",
-      title:"埃罗芒阿老师",
-      img:{
-        post_img:"/images/xjj.png",
-        author_img:"/images/gkls.jpg",
+
+    var post_content = [
+      {
+        data: "Sep 18 2016",
+        title: "埃罗芒阿老师",
+        post_img: "/images/xjj.png",
+        author_img: "/images/gkls.jpg",
+        img_condition: true,
+        content: "《情色漫画老师》是日本轻小说作家伏见司继《我的妹妹哪有这么可爱！》完结之后的轻小说新作，插画同样由负责《我的妹妹哪有这么可爱！》插画的神崎广绘制。",
+        view_num: "112",
+        collect_num: "96",
       },
-      img_condition: true,
-      content:"《情色漫画老师》是日本轻小说作家伏见司继《我的妹妹哪有这么可爱！》完结之后的轻小说新作，插画同样由负责《我的妹妹哪有这么可爱！》插画的神崎广绘制。",
-      view_num:"112",
-      collect_num:"96",
-    }
-    this.setData(post_content1);
+      {
+        data: "Nov 25 2016",
+        title: "正确的KADO",
+        post_img: "/images/xjj.png",
+        author_img: "/images/kado.jpg",
+        img_condition: true,
+        content: "《正解的卡多》是由东映动画原作并负责制作，村田和也指导，野崎窗系列构成并脚本的原创CG电视动画。",
+        view_num: "152",
+        collect_num: "56",
+      }
+    ] 
+
+    this.setData({post_key:post_content});
+
+
+
   },
 
   /**
