132bb85464078c7b0cb77029a41a5c7a72efc05c
萧俊介 <Alrcly@foxmail.com>
2017-06-07 16:03:18 +0800

    小程序的模板化与模块化

---
+++ Dome1/data/posts-data.js
@@ -0,0 +1,78 @@
+var local_database = [
+  {
+    data: "Sep 18 2016",
+    title: "埃罗芒阿老师",
+    avatar: "/images/xjj.png",
+    imgSrc: "/images/gkls.jpg",
+    content: "《情色漫画老师》是日本轻小说作家伏见司继《我的妹妹哪有这么可爱！》完结之后的轻小说新作，插画同样由负责《我的妹妹哪有这么可爱！》插画的神崎广绘制。",
+    reading: "112",
+    collection: "96",
+  },
+  {
+    data: "Nov 25 2016",
+    title: "正确的KADO",
+    avatar: "/images/xjj.png",
+    imgSrc: "/images/kado.jpg",
+    content: "《正解的卡多》是由东映动画原作并负责制作，村田和也指导，野崎窗系列构成并脚本的原创CG电视动画。",
+    reading: "152",
+    collection: "96",
+  },
+  {
+    data: "Sep 18 2016",
+    title: "埃罗芒阿老师2",
+    avatar: "/images/xjj.png",
+    imgSrc: "/images/gkls.jpg",
+    content: "《情色漫画老师》是日本轻小说作家伏见司继《我的妹妹哪有这么可爱！》完结之后的轻小说新作，插画同样由负责《我的妹妹哪有这么可爱！》插画的神崎广绘制。",
+    reading: "112",
+    collection: "96",
+  },
+  {
+    data: "Nov 25 2016",
+    title: "正确的KADO3",
+    avatar: "/images/xjj.png",
+    imgSrc: "/images/kado.jpg",
+    content: "《正解的卡多》是由东映动画原作并负责制作，村田和也指导，野崎窗系列构成并脚本的原创CG电视动画。",
+    reading: "152",
+    collection: "96",
+  },
+  {
+    data: "Sep 18 2016",
+    title: "埃罗芒阿老师3",
+    avatar: "/images/xjj.png",
+    imgSrc: "/images/gkls.jpg",
+    content: "《情色漫画老师》是日本轻小说作家伏见司继《我的妹妹哪有这么可爱！》完结之后的轻小说新作，插画同样由负责《我的妹妹哪有这么可爱！》插画的神崎广绘制。",
+    reading: "112",
+    collection: "96",
+  },
+  {
+    data: "Nov 25 2016",
+    title: "正确的KADO3",
+    avatar: "/images/xjj.png",
+    imgSrc: "/images/kado.jpg",
+    content: "《正解的卡多》是由东映动画原作并负责制作，村田和也指导，野崎窗系列构成并脚本的原创CG电视动画。",
+    reading: "152",
+    collection: "96",
+  },
+  {
+    data: "Sep 18 2016",
+    title: "埃罗芒阿老师4",
+    avatar: "/images/xjj.png",
+    imgSrc: "/images/gkls.jpg",
+    content: "《情色漫画老师》是日本轻小说作家伏见司继《我的妹妹哪有这么可爱！》完结之后的轻小说新作，插画同样由负责《我的妹妹哪有这么可爱！》插画的神崎广绘制。",
+    reading: "112",
+    collection: "96",
+  },
+  {
+    data: "Nov 25 2016",
+    title: "正确的KADO4",
+    avatar: "/images/xjj.png",
+    imgSrc: "/images/kado.jpg",
+    content: "《正解的卡多》是由东映动画原作并负责制作，村田和也指导，野崎窗系列构成并脚本的原创CG电视动画。",
+    reading: "152",
+    collection: "96",
+  }
+] 
+
+module.exports = {
+  postList: local_database
+}
